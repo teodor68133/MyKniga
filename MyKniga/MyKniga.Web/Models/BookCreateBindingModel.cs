ee170d0075175893c6349c4a4b8c869165583113
Vladimir Ivanov <35622848+lordinorbg@users.noreply.github.com>
2019-03-22 23:37:07 +0200

    Use new properties in book creation

--- MyKniga/MyKniga.Web/Models/BookCreateBindingModel.cs
+++ MyKniga/MyKniga.Web/Models/BookCreateBindingModel.cs
@@ -17,5 +17,34 @@ namespace MyKniga.Web.Models
         [Required]
         [Range(typeof(decimal), "0", "10000")]
         public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Year { get; set; }
+
+        [Required]
+        [MaxLength(1500)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        [Display(Name = "Short Description")]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Pages { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        [Url]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(13)]
+        [RegularExpression(@"^\d{13}$")]
+        [Display(Name = "ISBN")]
+        public string Isbn { get; set; }
     }
 }
