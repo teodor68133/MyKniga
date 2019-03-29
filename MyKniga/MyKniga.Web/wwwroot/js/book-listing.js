86bd42126631e72a63325fa14432efe1217e52b1
Teodor Zlatkov <35622940+teodor68133@users.noreply.github.com>
2019-03-29 22:00:49 +0200

    Fix code injection

--- MyKniga/MyKniga.Web/wwwroot/js/book-listing.js
+++ MyKniga/MyKniga.Web/wwwroot/js/book-listing.js
@@ -5,26 +5,27 @@ function refreshBooks() {
     }).done(function (response) {
         let bookHtml = '';
         response.forEach(function (book) {
-            bookHtml += 
+            bookHtml +=
                 `<div class="col-6 col-md-4 col-lg-3">
                     <div class="card m-1 shadow">
-                        <img src="${book.imageUrl}" class="card-img-top book-thumbnail-img" alt="...">
+                        <img src="${escapeHtml(book.imageUrl)}" class="card-img-top book-thumbnail-img" 
+                             alt="${escapeHtml(book.title)}">
                         <div class="card-body">
-                            <h6 class="card-title">${book.title}</h6>
+                            <h6 class="card-title">${escapeHtml(book.title)}</h6>
                             <p class="card-text">€${book.price}
                                 <br>
-                                ${book.author}
+                                ${escapeHtml(book.author)}
                             </p>
                             <a href="/books/details/${book.id}" class="btn btn-link stretched-link">Details</a>
                         </div>
                     </div>
                 </div>`;
         });
-        
+
         $('#booksList').html(bookHtml);
     });
 }
 
-(function() {
+(function () {
     refreshBooks();
 })();
