0b46b65d493b36a62df5bf1e89333e6dccb329a6
Ivo Ivanov <35624522+user3201@users.noreply.github.com>
2019-03-29 21:31:37 +0200

    Implement publisher creation

--- MyKniga/MyKniga.Common/NotificationMessages.cs
+++ MyKniga/MyKniga.Common/NotificationMessages.cs
@@ -8,5 +8,7 @@ namespace MyKniga.Common
         public const string BookCreateSuccessMessage = "Book created successfully";
         public const string PurchaseErrorMessage = "An error occured while purchasing book";
         public const string PurchaseSuccessMessage = "Successfully purchased book";
+        public const string PublisherCreateErrorMessage = "An error occured while creating publisher";
+        public const string PublisherCreateSuccessMessage = "Successfully added new publisher";
     }
 }
---
+++ MyKniga/MyKniga.Web/Controllers/PublishersController.cs
@@ -0,0 +1,50 @@
+namespace MyKniga.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Common;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.Interfaces;
+    using Services.Models;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class PublishersController : BaseController
+    {
+        private readonly IPublishersService publishersService;
+
+        public PublishersController(IPublishersService service)
+        {
+            this.publishersService = service;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PublisherCreateBindingModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                this.ShowErrorMessage(NotificationMessages.PublisherCreateErrorMessage);
+                return this.View(model);
+            }
+
+            var servicePublisher = Mapper.Map<PublisherCreateServiceModel>(model);
+
+            var success = await this.publishersService.CreatePublisherAsync(servicePublisher);
+
+            if (!success)
+            {
+                this.ShowErrorMessage(NotificationMessages.PublisherCreateErrorMessage);
+                return this.View(model);
+            }
+
+            this.ShowSuccessMessage(NotificationMessages.PublisherCreateSuccessMessage);
+            return this.RedirectToAction("Index", "Home");
+        }
+    }
+}
