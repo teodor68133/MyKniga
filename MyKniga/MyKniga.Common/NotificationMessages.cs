d6d2aaf75826463c4bafe7ecf964ea2d83467962
Teodor Zlatkov <35622940+teodor68133@users.noreply.github.com>
2019-03-29 23:01:38 +0200

    Fix typos

--- MyKniga/MyKniga.Common/NotificationMessages.cs
+++ MyKniga/MyKniga.Common/NotificationMessages.cs
@@ -2,13 +2,13 @@ namespace MyKniga.Common
 {
     public static class NotificationMessages
     {
-        public const string TagCreateErrorMessage = "An error occured while creating tag";
+        public const string TagCreateErrorMessage = "An error occurred while creating tag";
         public const string TagCreateSuccessMessage = "Tag created successfully";
-        public const string BookCreateErrorMessage = "An error occured while creating book";
+        public const string BookCreateErrorMessage = "An error occurred while creating book";
         public const string BookCreateSuccessMessage = "Book created successfully";
-        public const string PurchaseErrorMessage = "An error occured while purchasing book";
+        public const string PurchaseErrorMessage = "An error occurred while purchasing book";
         public const string PurchaseSuccessMessage = "Successfully purchased book";
-        public const string PublisherCreateErrorMessage = "An error occured while creating publisher";
+        public const string PublisherCreateErrorMessage = "An error occurred while creating publisher";
         public const string PublisherCreateSuccessMessage = "Successfully added new publisher";
     }
 }
