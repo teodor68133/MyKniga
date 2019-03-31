dc0843205bc4839db3fd4b30e19765c6e522b005
Vladimir Ivanov <35622848+lordinorbg@users.noreply.github.com>
2019-03-31 23:20:41 +0300

    Add tests for GetBookByIdAsync and AddTagToBookAsync

--- MyKniga/MyKniga.Tests/BooksServiceTests.cs
+++ MyKniga/MyKniga.Tests/BooksServiceTests.cs
@@ -119,5 +119,138 @@ namespace MyKniga.Tests
             // Assert
             Assert.Empty(actualResult);
         }
+
+        [Fact]
+        public async Task GetBookByIdAsync_WithCorrectId_WorksCorrectly()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var booksService = new BooksService(context);
+            var expectedBook = new Book {Title = "Book1"};
+
+            await context.Books.AddRangeAsync(expectedBook, new Book
+            {
+                Title = "Book2"
+            });
+
+            await context.SaveChangesAsync();
+
+            // Act
+            var actualBook = await booksService.GetBookByIdAsync<BookDetailsServiceModel>(expectedBook.Id);
+
+            // Assert
+            Assert.NotNull(actualBook);
+            Assert.Equal(expectedBook.Title, actualBook.Title);
+        }
+        
+        [Fact]
+        public async Task GetBookByIdAsync_WithIncorrectId_ReturnsNull()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var booksService = new BooksService(context);
+            var testId = Guid.NewGuid().ToString();
+
+            await context.Books.AddAsync(new Book {Title = "Book1"});
+
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await booksService.GetBookByIdAsync<BookDetailsServiceModel>(testId);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task AddTagToBookAsync_WithCorrectIds_WorksCorrectly()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var booksService = new BooksService(context);
+            var tag = new Tag();
+            var book = new Book();
+
+            await context.Tags.AddAsync(tag);
+            await context.Books.AddAsync(book);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await booksService.AddTagToBookAsync(book.Id, tag.Id);
+            
+            // Assert
+            Assert.True(result);
+            Assert.True(await context.BookTags.AnyAsync(bt => bt.BookId == book.Id && bt.TagId == tag.Id));
+        }
+        
+        [Fact]
+        public async Task AddTagToBookAsync_WithIncorrectTagId_ReturnsFalse()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var booksService = new BooksService(context);
+            var tag = new Tag();
+            var book = new Book();
+            var nonexistentTagId = Guid.NewGuid().ToString();
+
+            await context.Tags.AddAsync(tag);
+            await context.Books.AddAsync(book);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await booksService.AddTagToBookAsync(book.Id, nonexistentTagId);
+            
+            // Assert
+            Assert.False(result);
+            Assert.False(await context.BookTags.AnyAsync(bt => bt.BookId == book.Id && bt.TagId == tag.Id));
+        }
+        
+        [Fact]
+        public async Task AddTagToBookAsync_WithIncorrectBookId_ReturnsFalse()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var booksService = new BooksService(context);
+            var tag = new Tag();
+            var book = new Book();
+            var nonexistentBookId = Guid.NewGuid().ToString();
+
+            await context.Tags.AddAsync(tag);
+            await context.Books.AddAsync(book);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await booksService.AddTagToBookAsync(nonexistentBookId, tag.Id);
+            
+            // Assert
+            Assert.False(result);
+            Assert.False(await context.BookTags.AnyAsync(bt => bt.BookId == book.Id && bt.TagId == tag.Id));
+        }
+        
+        [Fact]
+        public async Task AddTagToBookAsync_WithExistingBookTagRelation_ReturnsFalse()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var booksService = new BooksService(context);
+            var tag = new Tag();
+            var book = new Book();
+
+            await context.Tags.AddAsync(tag);
+            await context.Books.AddAsync(book);
+            await context.BookTags.AddAsync(new BookTag()
+            {
+                BookId = book.Id,
+                TagId = tag.Id
+            });
+            
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await booksService.AddTagToBookAsync(book.Id, tag.Id);
+            
+            // Assert
+            Assert.False(result);
+        }
     }
 }
