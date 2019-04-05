f72e5ec3e125d84a07c19e2a7894692e4e737036
Vladimir Ivanov <35622848+lordinorbg@users.noreply.github.com>
2019-04-05 23:55:38 +0300

    Add tests for GetAllTagsAsync and RemoveTagAsync

--- MyKniga/MyKniga.Tests/TagsServiceTests.cs
+++ MyKniga/MyKniga.Tests/TagsServiceTests.cs
@@ -1,5 +1,6 @@
-namespace MyKniga.Tests
+﻿namespace MyKniga.Tests
 {
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
@@ -123,6 +124,114 @@ namespace MyKniga.Tests
 
             // Assert
             Assert.Empty(actualResult);
+        }
+
+        [Fact]
+        public async Task GetAllTagsAsync_WithQuery_WorksCorrectly()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var expectedTagNames = new[] {"cat", "caterpillar"};
+
+            await context.AddRangeAsync(
+                new Tag
+                {
+                    Name = "dog"
+                },
+                new Tag
+                {
+                    Name = "caterpillar"
+                },
+                new Tag
+                {
+                    Name = "cat"
+                });
+
+            await context.SaveChangesAsync();
+
+            var tagsService = new TagsService(context);
+
+            // Act
+            var returnedTagNames = (await tagsService.GetAllTagsAsync("cat"))
+                .Select(t => t.Name)
+                .ToArray();
+
+            // Assert
+            Assert.NotNull(returnedTagNames);
+            Assert.Equal(2, returnedTagNames.Length);
+            Assert.Equal(expectedTagNames, returnedTagNames);
+        }
+
+        [Fact]
+        public async Task DeleteTagAsync_WithCorrectData_WorksCorrectly()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var tag = new Tag
+            {
+                Name = "someTag"
+            };
+
+            await context.Tags.AddAsync(tag);
+
+            await context.SaveChangesAsync();
+
+            var tagsService = new TagsService(context);
+
+            // Act
+            var result = await tagsService.DeleteTagAsync(tag.Id);
+
+            // Assert
+            Assert.True(result);
+            Assert.False(await context.Tags.AnyAsync());
+        }
+
+        [Fact]
+        public async Task DeleteTagAsync_WithNullTagId_ReturnsFalse()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var tag = new Tag
+            {
+                Name = "someTag"
+            };
+
+            await context.Tags.AddAsync(tag);
+
+            await context.SaveChangesAsync();
+
+            var tagsService = new TagsService(context);
+
+            // Act
+            var result = await tagsService.DeleteTagAsync(null);
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(1, await context.Tags.CountAsync());
+        }
+
+        [Fact]
+        public async Task DeleteTagAsync_WithInvalidTagId_ReturnsFalse()
+        {
+            // Arrange
+            var context = this.NewInMemoryDatabase();
+            var testTagId = Guid.NewGuid().ToString();
+
+            await context.Tags.AddAsync(new Tag
+            {
+                Name = "someTag"
+            });
+
+            await context.SaveChangesAsync();
+
+            var tagsService = new TagsService(context);
+
+            // Act
+            var result = await tagsService.DeleteTagAsync(testTagId);
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(1, await context.Tags.CountAsync());
         }
     }
 }
