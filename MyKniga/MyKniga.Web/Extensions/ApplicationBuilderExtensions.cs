576304b88289162eb48cd9b305c6cf90df4ce484
Teodor Zlatkov <35622940+teodor68133@users.noreply.github.com>
2019-04-06 18:46:13 +0300

    Add initial administrator

--- MyKniga/MyKniga.Web/Extensions/ApplicationBuilderExtensions.cs
+++ MyKniga/MyKniga.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -7,6 +7,8 @@ namespace MyKniga.Web.Extensions
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
     using Data;
+    using Microsoft.EntityFrameworkCore.Internal;
+    using MyKniga.Models;
 
     public static class ApplicationBuilderExtensions
     {
@@ -24,6 +26,8 @@ namespace MyKniga.Web.Extensions
 
                 await dbContext.Database.MigrateAsync();
 
+                // Create administrator and publisher roles
+
                 var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
 
                 if (!await roleManager.RoleExistsAsync(GlobalConstants.AdministratorRoleName))
@@ -34,6 +38,22 @@ namespace MyKniga.Web.Extensions
                 if (!await roleManager.RoleExistsAsync(GlobalConstants.PublisherRoleName))
                 {
                     await roleManager.CreateAsync(new IdentityRole(GlobalConstants.PublisherRoleName));
+                }
+
+                // Create initial administrator
+
+                var userManager = serviceScope.ServiceProvider.GetService<UserManager<KnigaUser>>();
+
+                if (!dbContext.Users.Any())
+                {
+                    var admin = new KnigaUser
+                    {
+                        Email = "admin@admin.com",
+                        UserName = "admin@admin.com"
+                    };
+
+                    await userManager.CreateAsync(admin, "admin123");
+                    await userManager.AddToRoleAsync(admin, GlobalConstants.AdministratorRoleName);
                 }
             }
         }
